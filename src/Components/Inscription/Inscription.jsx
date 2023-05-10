396545f62d80187c486cde262377607d42c7eb8c
The princetoutcouleur <babathiam0000@gmail.com>
2023-05-10 15:07:15 +0000

    scroll inscription

--- src/Components/Inscription/Inscription.jsx
+++ src/Components/Inscription/Inscription.jsx
@@ -6,7 +6,7 @@ import logo from "../../Assets/images/bakeli.png";
 
 const Inscription = () => {
   return (
-    <div className="container mt-5">
+    <div className="container">
       <div className="row">
         <div className="col-lg-12">
           <center>
